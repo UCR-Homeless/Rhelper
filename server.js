e75d1366dcbc63f919d08bb1b7cd9d8ce2a08eb3
Jia Xie <jxie031@ucr.edu>
2019-10-28 19:14:59 -0700

    Update server.js

--- server.js
+++ server.js
@@ -3,18 +3,21 @@
 module.exports.sql_start = () =>{
 var express    = require("express");
  var mysql      = require('mysql');
- var connection = mysql.createConnection({
+ var app = express();
+ var path = require("path");
+app.get('/',function(req,res){
+	  res.sendFile(path.join('/var/www/html/google.html'));
+	  //__dirname : It will resolve to your project folder.
+});
+app.get('/up',function(req,res){
+	var connection = mysql.createConnection({
 	    host     : 'localhost',
 	    user     : 'root',
 	    password : 'rhelper',
 	    database : 'mysql',
 	    socketPath: '/var/run/mysqld/mysqld.sock'
 	  });
- var app = express();
- var path = require("path");
-app.get('/',function(req,res){
-	  res.sendFile(path.join('/var/www/html/google.html'));
-	  //__dirname : It will resolve to your project folder.
+	  
 });
 app.listen(3000);
 console.log("Server running at Port 3000");
